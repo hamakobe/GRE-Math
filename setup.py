2414c1257d993ff6b9cc5a036625d18138b3ac59
Harmon Amakobe <hamakobe@users.noreply.github.com>
2017-11-19 15:13:46 -0500

    Directory Structure
    Updated the directory structure to industry standard. Also created setup.py file and initial hello world programs.

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+setup(
+    name='Q',
+    version='0.0.1',
+    description='Functions for calculating GRE level math',
+    long_description='Displayed on GitHub',
+    url='https://github.com/hamakobe/GRE-Math',
+    author='Harmon Amakobe',
+    author_email='hamakobe@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers'
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6'
+        ],
+    keywords='GRE math functions',
+    packages=find_packages(exclude = ['docs', 'tests*']),
+    install_requires=['pymongo']
+    package_data={
+        'Q' : ['random_data.dat']
+        },
+    data_files=None,
+    entry_points={
+        'console_scripts': [
+            'hello=Q:say_hello',
+            ],
+        }
+    )
---
+++ tests/test_Q.py
@@ -0,0 +1,10 @@
+import Q
+
+class TestQ(unittest.TestCase):
+    
+    def setUp(self):
+        self.hello_message = "Hello, Q User!"
+        
+    def test_prints_hello_Q(self):
+        output = Q.hello()
+        assert(output) == self.hello_message)
