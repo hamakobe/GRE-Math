56b924173554702c812c8c1c588d495d44825218
Harmon Amakobe <hamakobe@users.noreply.github.com>
2017-11-20 00:59:20 -0500

    Going for 100
    Added test functions in order to ensure that quantv is fully covered.

--- tests/test_quantv.py
+++ tests/test_quantv.py
@@ -8,5 +8,17 @@ class TestQuantV(unittest.TestCase):
         self.hello_message = "Hello, Q User!"
         
     def test_prints_hello_QuantV(self):
+        quantv.say_hello()
+        
+    def test_prints_sayhello(self):
         output = quantv.hello()
-        assert(output == self.hello_message)
+        assert(output == self.hello_message)   
+        
+    def test_isEven(self):
+        output = quantv.is_even(4)
+        assert(output == True)
+        
+    def test_isNotEven(self):
+        output = quantv.is_even(5)
+        assert(output == False)
+        
